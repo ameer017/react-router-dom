7f0f5eb98d7a99bd5946f8e1d850bd46a18bd427
mac <mac@macs-MacBook-Air-2.local>
2023-11-02 15:10:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,154 @@
+import { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import { format } from "date-fns";
+
+
+import Home from "./Home";
+import NewPost from "./NewPost";
+import PostPage from "./PostPage";
+import Missing from "./Missing";
+import About from "./About";
+import HomeLayout from "./HomeLayout";
+import api from './api/posts'
+import EditPost from "./EditPost";
+
+const App = () => {
+  // step 1 move the posts to a new folder -data/db.json
+  const [posts, setPosts] = useState([]);
+  const [searchResult, setSearchResult] = useState([]);
+  const [search, setSearch] = useState("");
+  const [postTitle, setPostTitle] = useState("");
+  const [postBody, setPostBody] = useState("");
+  const [editTitle, setEditTitle] = useState("");
+  const [editBody, setEditBody] = useState("");
+
+  // step 2- structure the useEffect
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await api.get('/posts');
+        setPosts(response.data);
+      } catch (error) {
+        if(error.response) {
+        // not in 200 response range
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else {
+        // 404 error
+        console.log(`Error: ${error.message}`)
+      }
+      }
+    }
+
+    fetchPosts()
+    // check if it shows the posts
+  }, [])
+
+  useEffect(() => {
+    const filterResult = posts.filter(post => 
+      post.body.toLowerCase().includes(search.toLowerCase()) 
+      || 
+      post.title.toLowerCase().includes(search.toLowerCase()))
+      setSearchResult(filterResult.reverse())
+
+  }, [posts, search])
+  
+
+  const navigate = useNavigate();
+
+
+
+  // step 3 - make the fn async, then manipulate the code
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
+    const date = format(new Date(), 'MMMM dd, yyyy pp');
+    const newPost = { id, title: postTitle, date, body: postBody}
+    try {
+      const response = await api.post('/posts', newPost)
+      const allPost = [...posts, response.data]
+      setPosts(allPost)
+      setPostTitle('')
+      setPostBody('')
+      navigate("/")
+      
+    } catch (error) {
+      console.log(`Error: ${error.message}`)
+    }
+  }
+
+    // step 4 
+    const handleDelete = async (id) => {
+      try {
+        await api.delete(`/posts/${id}`);
+        const postList = posts.filter((post) => post.id !== id);
+        setPosts(postList);
+        navigate("/");
+        
+      } catch (error) {
+        console.log(`Error: ${error.message}`)
+      }
+    };
+
+    // step 5
+    const handleEdit = async (id) => {
+      const date = format(new Date(), 'MMMM dd, yyyy pp');
+      const updatedPost = { id, title: editTitle, date, body: editBody };
+      try {
+        const response = await api.put(`/posts/${id}`, updatedPost);
+        setPosts(posts.map(post => post.id === id ? { ...response.data } : post));
+        setEditTitle('');
+        setEditBody('');
+        navigate('/');
+      } catch (err) {
+        console.log(`Error: ${err.message}`);
+      }
+    }
+
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={<HomeLayout search={search} setSearch={setSearch} />}
+      >
+        <Route index element={<Home posts={searchResult} />} />
+        <Route path="/post">
+          <Route
+            index
+            element={
+              <NewPost
+                postTitle={postTitle}
+                setPostTitle={setPostTitle}
+                postBody={postBody}
+                setPostBody={setPostBody}
+                handleSubmit={handleSubmit}
+              />
+            }
+          />
+          
+          <Route
+            path=":id"
+            element={<PostPage posts={posts} handleDelete={handleDelete} />}
+          />
+        </Route>
+        <Route
+            path="/edit/:id"
+            element={
+              <EditPost
+                editTitle={editTitle}
+                setEditTitle={setEditTitle}
+                editBody={editBody}
+                setEditBody={setEditBody}
+                handleEdit={handleEdit}
+              />
+            }
+          />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
